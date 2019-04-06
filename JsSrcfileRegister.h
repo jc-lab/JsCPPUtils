5b98fdf738f82d50d6ad72c508a207d325f8b96c
Jichan <development@jc-lab.net>
2019-04-06 10:21:48 +0900

    Fix c++ static initialize order problem

--- JsSrcfileRegister.h
+++ JsSrcfileRegister.h
@@ -18,12 +18,25 @@
 #include <string>
 #include <map>
 
+#include "SmartPointer.h"
+
 namespace JsCPPUtils {
 	class JsSrcfileRegisterBase
 	{
 	public:
-		struct ModuleBase {
+		class ModuleBase {
+		private:
+			std::map<void*, JsCPPUtils::SmartPointer<JsSrcfileRegisterBase> > providers;
+
+		public:
 			std::basic_string<char> __JSFR_name;
+
+			void addProvider(JsCPPUtils::SmartPointer<JsSrcfileRegisterBase> provider) {
+				providers[(void*)provider.getPtr()] = provider;
+			}
+			void removeProvider(JsCPPUtils::SmartPointer<JsSrcfileRegisterBase> provider) {
+				providers.erase((void*)provider.getPtr());
+			}
 		};
 
 		typedef std::map<std::basic_string<char>, ModuleBase*> ModuleListBaseType;
@@ -50,28 +63,33 @@ namespace JsCPPUtils {
 
 #define JsSrcfileRegister_PROVIDERCLASS(PROVIDER) _JsCPPUtils_private::__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)
 #define JsSrcfileRegister_PROVIDER(PROVIDER) _JsCPPUtils_private::__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)*
-#define JsSrcfileRegister_getProvider(PROVIDER) _JsCPPUtils_private::__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)::getInstance()
+#define JsSrcfileRegister_getProvider(PROVIDER) _JsCPPUtils_private::__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)::getInstance().getPtr()
 #define JsSrcfileRegister_getModuleList(PROVIDERCLASS) (PROVIDERCLASS)->m_modules
 #define JsSrcfileRegister_PROVIDERMODULETYPE(PROVIDER) JsSrcfileRegister_PROVIDERCLASS(PROVIDER)::Module*
+
+class deletertest
+{
+public:
+	void operator()(void *ptr)
+	{
+		printf("DELETER : %p\n", ptr);
+	}
+};
 
 #define JsSrcfileRegister_PROVIDER_DECLARE(PROVIDER, TDATASTRUCTURE) \
 namespace _JsCPPUtils_private { \
 	class __JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER) : public JsCPPUtils::JsSrcfileRegisterBase { \
 	public: \
 		struct Module : public ModuleBase { TDATASTRUCTURE data; Module(const TDATASTRUCTURE &_data) : data(_data){ } }; \
-	private: \
-		static __JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)* m_instance; \
-		static void _init(){ if(!m_instance){ m_instance = new  __JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)(); } } \
 	public: \
-		static __JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)* getInstance(){ _init(); return m_instance; } \
-		static void _Sregister(Module *pModule) { _init(); m_instance->_register(pModule); } \
-		static void _Sunregister(Module *pModule) { m_instance->_unregister(pModule); } \
+		static JsCPPUtils::SmartPointer<__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)> getInstance(){ static JsCPPUtils::SmartPointer<__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)> instance = new  __JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)(); return instance; } \
+		static void _Sregister(Module *pModule) { JsCPPUtils::SmartPointer<__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)> instance = getInstance(); pModule->addProvider(instance); instance->_register(pModule); } \
+		static void _Sunregister(Module *pModule) { JsCPPUtils::SmartPointer<__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)> instance = getInstance(); instance->_unregister(pModule); pModule->removeProvider(instance); } \
 	}; \
 }
 
 #define JsSrcfileRegister_PROVIDER_DEFINE(PROVIDER) \
 namespace _JsCPPUtils_private { \
-	__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER) *__JSCPPUTILS_JSSRCFILEREISTER_PROVIDERCLASS(PROVIDER)::m_instance = NULL; \
 }
 
 #define __JSCPPUTILS_JSSRCFILEREGISTER_BRACED_INIT_LIST(...) {__VA_ARGS__}
